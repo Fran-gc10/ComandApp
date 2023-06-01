f05ccdb9afe0c42144b5698d300553382fc550b9
Fran-gc10 <gutierrez.castro.1994@gmail.com>
2023-06-01 12:04:32 +0200

    repository_fix

--- Back/ComandApp/src/main/java/Proyecto/ComandApp/repositories/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package Proyecto.ComandApp.repositories;
-
-import Proyecto.ComandApp.security.entity.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-//@Repository
-//public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
-//}
